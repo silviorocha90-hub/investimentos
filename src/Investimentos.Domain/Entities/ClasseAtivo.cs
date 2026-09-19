0f7e3fb1ac0b98e8e730051792673f92b142d0e2
silviorocha90-hub <silviorocha90@gmail.com>
2026-09-19 13:17:23 -0300

    Parâmetros: permite atualizar classe do ativo

--- src/Investimentos.Domain/Entities/ClasseAtivo.cs
+++ src/Investimentos.Domain/Entities/ClasseAtivo.cs
@@ -30,6 +30,25 @@
             Codigo = codigo.Trim().ToUpperInvariant();
             Nome = nome.Trim();
             Ativo = true;
+
+        public void Atualizar(
+            string codigo,
+            string nome,
+            bool ativo)
+        {
+            if (string.IsNullOrWhiteSpace(codigo))
+                throw new ArgumentException(
+                    "O código do classe do ativo é obrigatório.");
+
+            if (string.IsNullOrWhiteSpace(nome))
+                throw new ArgumentException(
+                    "O nome do classe do ativo é obrigatório.");
+
+            Codigo =
+                codigo.Trim().ToUpperInvariant();
+            Nome = nome.Trim();
+            Ativo = ativo;
+        }
         }
     }
 }
