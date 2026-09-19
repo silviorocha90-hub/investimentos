44e2e58ccc98e5563372f6648fed126f70430e51
silviorocha90-hub <silviorocha90@gmail.com>
2026-09-19 13:54:23 -0300

    EF: corrige designer da migration patrimonial

--- src/Investimentos.Infrastructure/Migrations/20260919160000_AdicionarValorPatrimonialAtivo.Designer.cs
+++ src/Investimentos.Infrastructure/Migrations/20260919160000_AdicionarValorPatrimonialAtivo.Designer.cs
@@ -2,7 +2,9 @@
 using Investimentos.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
@@ -16,8 +18,57 @@ namespace Investimentos.Infrastructure.Migrations
             ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            new InvestimentosDbContextModelSnapshot()
-                .BuildModel(modelBuilder);
+            modelBuilder
+                .HasAnnotation("ProductVersion", "10.0.11")
+                .HasAnnotation("Relational:MaxIdentifierLength", 128);
+
+            SqlServerModelBuilderExtensions.UseIdentityColumns(
+                modelBuilder);
+
+            modelBuilder.Entity(
+                "Investimentos.Domain.Entities.ValorPatrimonialAtivo",
+                b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("uniqueidentifier");
+
+                    b.Property<Guid>("AtivoId")
+                        .HasColumnType("uniqueidentifier");
+
+                    b.Property<DateTime>("DataReferencia")
+                        .HasColumnType("datetime2");
+
+                    b.Property<decimal>("Valor")
+                        .HasPrecision(18, 2)
+                        .HasColumnType("decimal(18,2)");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex(
+                            "AtivoId",
+                            "DataReferencia")
+                        .IsUnique();
+
+                    b.ToTable(
+                        "ValorPatrimonialAtivo",
+                        (string)null);
+                });
+
+            modelBuilder.Entity(
+                "Investimentos.Domain.Entities.ValorPatrimonialAtivo",
+                b =>
+                {
+                    b.HasOne(
+                            "Investimentos.Domain.Entities.Ativo",
+                            "Ativo")
+                        .WithMany()
+                        .HasForeignKey("AtivoId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Ativo");
+                });
 #pragma warning restore 612, 618
         }
     }
