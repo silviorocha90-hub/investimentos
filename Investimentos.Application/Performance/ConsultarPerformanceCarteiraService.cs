77d48061a83047f75927cde8c66f3e70dfb5332d
silviorocha90-hub <silviorocha90@gmail.com>
2026-09-25 18:14:52 -0300

    Consolida histórico com último patrimônio conhecido

--- Investimentos.Application/Performance/ConsultarPerformanceCarteiraService.cs
+++ Investimentos.Application/Performance/ConsultarPerformanceCarteiraService.cs
@@ -32,18 +32,40 @@ public class ConsultarPerformanceCarteiraService
                 movimentacoes);
         }
 
-        var historicoConsolidado = historicos
-            .GroupBy(x => x.DataReferencia.Date)
-            .Select(grupo =>
-            {
-                var primeiro = grupo.First();
-                return new HistoricoPatrimonio(
-                    primeiro.Investidor,
-                    grupo.Key,
-                    grupo.Sum(x => x.ValorCarteira));
-            })
-            .OrderBy(x => x.DataReferencia)
+        var datas = historicos
+            .Select(x => x.DataReferencia.Date)
+            .Distinct()
+            .OrderBy(x => x)
             .ToList();
+
+        var porInvestidor = historicos
+            .GroupBy(x => x.InvestidorId)
+            .ToList();
+
+        var historicoConsolidado = new List<HistoricoPatrimonio>();
+
+        foreach (var data in datas)
+        {
+            var valoresDisponiveis = porInvestidor
+                .Select(grupo => grupo
+                    .Where(x => x.DataReferencia.Date <= data)
+                    .OrderByDescending(x => x.DataReferencia)
+                    .FirstOrDefault())
+                .Where(x => x is not null)
+                .ToList();
+
+            if (valoresDisponiveis.Count == 0)
+            {
+                continue;
+            }
+
+            var referencia = valoresDisponiveis[0]!;
+            historicoConsolidado.Add(
+                new HistoricoPatrimonio(
+                    referencia.Investidor,
+                    data,
+                    valoresDisponiveis.Sum(x => x!.ValorCarteira)));
+        }
 
         return CalculadoraPerformanceCarteira.Calcular(
             historicoConsolidado,
