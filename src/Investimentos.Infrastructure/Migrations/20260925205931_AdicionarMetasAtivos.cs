05af1b777a6e3b03fca6b353e279817de4e80bcf
Lucas Rocha <ljerocha@gmail.com>
2026-09-25 18:03:54 -0300

    Adiciona migration de metas por ativo

---
+++ src/Investimentos.Infrastructure/Migrations/20260925205931_AdicionarMetasAtivos.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Investimentos.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionarMetasAtivos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MetaAtivo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    InvestidorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AtivoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    QuantidadeDesejada = table.Column<decimal>(type: "decimal(18,4)", precision: 18, scale: 4, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MetaAtivo", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MetaAtivo_Ativo_AtivoId",
+                        column: x => x.AtivoId,
+                        principalTable: "Ativo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MetaAtivo_Investidor_InvestidorId",
+                        column: x => x.InvestidorId,
+                        principalTable: "Investidor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MetaAtivo_AtivoId",
+                table: "MetaAtivo",
+                column: "AtivoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MetaAtivo_InvestidorId_AtivoId",
+                table: "MetaAtivo",
+                columns: new[] { "InvestidorId", "AtivoId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MetaAtivo");
+        }
+    }
+}
