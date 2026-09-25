3693318a4d5c37e0fed2a51ab6582abee2bd4407
silviorocha90-hub <silviorocha90@gmail.com>
2026-09-25 18:14:24 -0300

    Expõe movimentações para performance

--- Investimentos.Application/Interfaces/IMovimentacaoFinanceiraRepository.cs
+++ Investimentos.Application/Interfaces/IMovimentacaoFinanceiraRepository.cs
@@ -4,6 +4,10 @@ namespace Investimentos.Application.Interfaces
 {
     public interface IMovimentacaoFinanceiraRepository
     {
+        Task<IReadOnlyList<MovimentacaoFinanceira>> ListarAsync(
+            Guid? investidorId = null,
+            CancellationToken cancellationToken = default);
+
         Task<IReadOnlyList<MovimentacaoFinanceira>> ListarAnoAsync(
             Guid investidorId,
             int ano,
